f20bb7267ccb7eedfb6a9bbbe21a77095cbac7f1
Nathan <nathan@calibrae.com>
2023-03-15 13:08:14 +0000

    Refactored the view code into app.js

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
-// setTimeout(
-//     () => alert(document.getElementById('title1').innerText),
-//     20
-// );
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+          
+            createApp({
 
-// alert(document.getElementById('title1').innerText);
+                data() {
+                    return {
+                        message: 'Welcome to the new and improved Netflix!',
+                        search: '',
+                        movieMatches: [],
+                        movies: [
+                            {
+                                title: 'The Matrix',
+                                poster: 'https://m.media-amazon.com/images/I/91++WV6FP4L._AC_SL1500_.jpg',
+                                directors: [
+                                    'Wachowski 1',
+                                    'Wachowski 2'
+                                ],
+                                actors: [
+                                    'Mr Reeves',
+                                    'Mr Fishbourne'
+                                ]
+                            },
+                            {
+                                title: 'Hitch',
+                                poster: 'https://i.ebayimg.com/images/g/DpAAAOSwKIpj9vpq/s-l500.jpg',
+                                directors: [
+                                    'Kevin'
+                                ],
+                                actors: [
+                                    'Mr Smith',
+                                    'Bob'
+                                ]
+                            }
+                        ]
+                    }
+                },
 
-function promptForRename(event) {
+                methods: {
 
-    // Prompt the user to rename the movie
-    const name = prompt('Rename movie', event.target.innerText);
+                    getMoviePosterStyle: movie => 'background-image: url(' + movie.poster + ')',
 
-    // Update the DOM so that the title has the given name
-    event.target.innerText = name;
-}
+                    renameTitle(movie) {
+                        movie.title = prompt('Rename movie', movie.title);
+                    },
 
-// bind the click listener to all renameable things
-function initClickHandlers() {
-    const renameables = document.getElementsByClassName('renameable');
-    for (let i = 0; i < renameables.length; i++) {
-        renameables[i].onclick = promptForRename;
-    }
-}
+                    doSearch() {
+                        // go through the movies and find the ones whose titles (and other?)
+                        // match the search term
+                        const searchLower = this.search.toLowerCase();
+                        this.movieMatches = this.movies.filter(
+                            movie => movie.title.toLowerCase().indexOf(searchLower) != -1
+                        );
 
-initClickHandlers();
-
-
-
-
+                    }
+                }
+            }).mount('#app')
